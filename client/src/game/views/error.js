33ec12b2bce96fc3ff9a40e4eac5c88d0f4cd2b9
Umer Kamran <ukamran@uwaterloo.ca>
2020-08-13 15:50:46 -0400

    Created a view for the error page

---
+++ client/src/game/views/error.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/* ----- Error Page ----- */
+
+function Error(props) {
+    return (
+        <React.Fragment>
+            <div className="error-title text text-center">
+                Error, please refer to the message below:
+            </div>
+            <div className="error-message text text-center">
+                {props.message}
+            </div>
+        </React.Fragment>
+    );
+}
