9e3bd67b59da009d3c96ca610125edc43ffa9767
Umer Kamran <ukamran@uwaterloo.ca>
2020-08-25 16:46:41 -0400

    Refactored the css to improve the modularity of the code

--- client/src/game/core.js
+++ client/src/game/core.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+/* ----- Third-Party Libraries ----- */
+
 import { ApolloProvider } from '@apollo/client';
 import { client } from './interfaces/client';
 
@@ -7,6 +9,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { Provider } from 'react-redux';
 import { store } from './interfaces/store';
+
+/* ----- React Component Pages ----- */
 
 import StartPage from './components/start';
 
@@ -19,7 +23,16 @@ import Results from './components/results';
 
 import GameOver from './components/game-over';
 
+/* ----- Cascading Style Sheets ----- */
+
 import './core.css';
+
+import './components/styles/text.css'
+import './components/styles/form.css'
+import './components/styles/table.css'
+
+import './components/styles/game.css'
+import './components/styles/pages.css'
 
 /* ----- Top-Level React Component ----- */
 
